15926498af6eef2b47a4cce4d0f0c5cb21f98cd8
sthapa-mbsys <sthapa.mbsys@gmail.com>
2021-10-07 15:07:42 +0545

    king twin size

--- src/Components/ErrorChecker/AttributeFormats.js
+++ src/Components/ErrorChecker/AttributeFormats.js
@@ -1,77 +1,77 @@
-export const AttributeFormats=function(){
+export const AttributeFormats = function () {
     return {
-        "attributeFormats":[
+        "attributeFormats": [
             {
-                name:"Weight",
-                space:true,
-                units:["kg","g","lb","mg","OZ"]
+                name: "Weight",
+                space: true,
+                units: ["kg", "g", "lb", "mg", "OZ"]
             },
             {
-                name:"Compatible Baby Weight",
-                space:true,
-                units:["kg"]
+                name: "Compatible Baby Weight",
+                space: true,
+                units: ["kg"]
             },
             {
-                name:"Capacity",
-                space:true,
-                units:["kg","oz","ltr","ml","ton","mAh"]
+                name: "Capacity",
+                space: true,
+                units: ["kg", "oz", "ltr", "ml", "ton", "mAh"]
             },
             {
-                name:"Quantity",
-                space:true,
-                units:["pc","g","ltr","ml","kg","sheets","pair","set","N","sticks","caps","tabs"]
+                name: "Quantity",
+                space: true,
+                units: ["pc", "g", "ltr", "ml", "kg", "sheets", "pair", "set", "N", "sticks", "caps", "tabs"]
             },
             {
-                name:"Size",
-                space:true,
-                shouldHaveNumber:'partial',
-                units:["mm","cm","inches","pulls","XS","S","M","L","XL","2XL","3XL"]
+                name: "Size",
+                space: true,
+                shouldHaveNumber: 'partial',
+                units: ["mm", "cm", "inches", "pulls", "XS", "S", "M", "L", "XL", "2XL", "3XL", "King Size", "Twin Size"]
             },
             {
-                name:"Screen Size",
-                space:true,
-                units:["inches"]
+                name: "Screen Size",
+                space: true,
+                units: ["inches"]
             },
             {
-                name:"Volume",
-                space:true,
-                units:["ml"]
+                name: "Volume",
+                space: true,
+                units: ["ml"]
             },
             {
-                name:"RAM",
-                space:true,
-                units:["GB"]
+                name: "RAM",
+                space: true,
+                units: ["GB"]
             },
             {
-                name:"Internal Storage",
-                space:true,
-                units:["GB"]  
+                name: "Internal Storage",
+                space: true,
+                units: ["GB"]
             },
             {
-                name:"Storage",
-                space:true,
-                units:["TB","GB"]
+                name: "Storage",
+                space: true,
+                units: ["TB", "GB"]
             },
             {
-                name:"Hard Disk Size",
-                space:true,
-                units:["TB","GB"]
+                name: "Hard Disk Size",
+                space: true,
+                units: ["TB", "GB"]
             },
             {
-                name:"Battery Capacity",
-                space:true,
-                units:["mAh"]
+                name: "Battery Capacity",
+                space: true,
+                units: ["mAh"]
             },
             {
-                name:"Battery",
-                space:true,
-                units:["mAh","Ah"]
+                name: "Battery",
+                space: true,
+                units: ["mAh", "Ah"]
             },
             {
-                name:"Resolution",
-                space:false,
-                units:["p","K"]
-            }            
+                name: "Resolution",
+                space: false,
+                units: ["p", "K"]
+            }
         ]
     }
 }
